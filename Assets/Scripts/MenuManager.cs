7cf4c8acd27fe8a3133713d178380925c816fdc1
NolSpencer <77764696+NolSpencer@users.noreply.github.com>
2022-10-27 17:07:05 -0400

    Scalable Shop Menu and small fixes
    Added Scalable Shop Menu (press Tab) its a little wonky with the controls. Added Mesh Colliders to some trees

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject shopMenu;
+    PlayerControls playerControls;
+    bool ShopButtonPressed = false;
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControls.player.ShopMenu.started += ShopButton;
+        playerControls.player.ShopMenu.canceled += ShopButton;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShopButtonPressed)
+        {
+            shopMenu.gameObject.SetActive(!shopMenu.gameObject.activeSelf);
+        }
+    }
+
+    void ShopButton(InputAction.CallbackContext ctx)
+    {
+        ShopButtonPressed = ctx.ReadValueAsButton();
+    }
+}
