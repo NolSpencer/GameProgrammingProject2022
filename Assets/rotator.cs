fc83c56893e2d940410ea395505c5b12470927ce
Josh Sakal <jsakal@kent.edu>
2022-12-11 22:22:57 -0500

    Changed some things, more details below

---
+++ Assets/rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    void Update()
+    {
+        if (transform != null)
+        {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
